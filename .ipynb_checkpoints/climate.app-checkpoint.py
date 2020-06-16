bee3603ca3193905640188faf98e2a425350d482
cflores1 <cflores1@email.arizona.edu>
2020-06-16 00:04:36 -0700

    update

---
+++ .ipynb_checkpoints/climate.app-checkpoint.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################
+# Database Setup
+#################
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
